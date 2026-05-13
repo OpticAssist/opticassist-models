aa41a1887deb51bffbad7455fe46c484e1411a03
ghxstly21 <migtav28@bergen.org>
2026-05-13 16:09:27 -0400

    Added eprint function to send errors to Rust

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import sys
+from typing import Literal
+
+
+# Prints error output to be collected in Rust
+def eprint(*values: object, sep: str | None = " ",
+           end: str | None = "\n",
+           flush: Literal[False] = False):
+    print(*values, sep=sep, end=end, flush=flush, file=sys.stderr)
+
+
 def main():
     print("hello, world!")
 
