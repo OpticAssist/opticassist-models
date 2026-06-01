c7759003195d1d3d8ca3a6d5a6462c9c50454271
DharmaZR <zalavadiyadharma@gmail.com>
2026-06-01 12:50:59 -0400

    Update build.py, added OS functionality for all platforms

--- build.py
+++ build.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
 
 import subprocess
+import os
 
 # get the onnx file
 subprocess.run(
-    [
-     "yolo",
-     "export",
-     "model=yolo26n.pt",
-     "format=onnx",
-     "opset=13",
-     "simplify=True"
-     ], check=True
-)
-
-# delete downloaded pt file
-subprocess.run(
  [
-  "rm",
-  "./yolo26n.pt"
+  "yolo",
+  "export",
+  "model=yolo26n.pt",
+  "format=onnx",
+  "opset=13",
+  "simplify=True"
  ], check=True
 )
 
-
-
-
-
+# delete downloaded pt file
+os.remove("./yolo26n.pt")
